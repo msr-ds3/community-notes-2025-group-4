6d0ed4e6a7c41be31faf8d6e494ceeb27350251e
ye-htut-maung <yehtutmaung21@gmail.com>
2025-06-19 23:03:46 -0400

    delete scripts from data folder

--- data/03_download_ratings_data.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# use curl download 20 rating data for the community notes, it will not redownload again if it already
-for i in $(seq -w 0 19); do
-  curl -O -z  ratings-000${i}.zip https://ton.twimg.com/birdwatch-public-data/2025/06/16/noteRatings/ratings-000${i}.zip
-done
